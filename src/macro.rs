d6f6112bb51ed8627135b51dbd66cf52f54e329e
kgv <kgv@users.noreply.github.com>
2026-05-29 15:18:39 +0300

    1

--- src/macro.rs
+++ src/macro.rs
@@ -5,15 +5,15 @@ macro_rules! asset {
     };
 }
 
-#[macro_export]
-macro_rules! l10n {
-    ($id:expr) => {
-        const_format::formatcp!("{}_{}", env!("CARGO_PKG_NAME"), $id)
-    };
-    ($id:expr; abbreviation) => {
-        const_format::formatcp!("{}_{}.abbreviation", env!("CARGO_PKG_NAME"), $id)
-    };
-    ($id:expr; hover) => {
-        const_format::formatcp!("{}_{}.hover", env!("CARGO_PKG_NAME"), $id)
-    };
-}
+// #[macro_export]
+// macro_rules! l10n {
+//     ($id:expr) => {
+//         const_format::formatcp!("{}_{}", env!("CARGO_PKG_NAME"), $id)
+//     };
+//     ($id:expr; abbreviation) => {
+//         const_format::formatcp!("{}_{}.abbreviation", env!("CARGO_PKG_NAME"), $id)
+//     };
+//     ($id:expr; hover) => {
+//         const_format::formatcp!("{}_{}.hover", env!("CARGO_PKG_NAME"), $id)
+//     };
+// }
