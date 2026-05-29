f82fc49164b3985146f3cfa806cdfa98d951bf6e
kgv <kgv@users.noreply.github.com>
2026-05-29 15:01:50 +0300

    Add asset macro

--- src/macro.rs
+++ src/macro.rs
@@ -1,4 +1,11 @@
 #[macro_export]
+macro_rules! asset {
+    ($path:literal) => {
+        include_str!(concat!(env!("CARGO_MANIFEST_DIR"), $path))
+    };
+}
+
+#[macro_export]
 macro_rules! l10n {
     ($id:expr) => {
         const_format::formatcp!("{}_{}", env!("CARGO_PKG_NAME"), $id)
