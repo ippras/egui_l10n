33cfbebec09ea2aeaefd5b85745531c0f7ac6134
kgv <kgv@users.noreply.github.com>
2026-05-29 18:49:23 +0300

    Delete ResponseExt

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub use self::{context::ContextExt, localization::Localization, response::ResponseExt, ui::UiExt};
+pub use self::{context::ContextExt, localization::Localization, ui::UiExt};
 pub use unic_langid::LanguageIdentifier;
 #[cfg(feature = "macros")]
 pub use unic_langid::langid;
@@ -6,7 +6,7 @@ pub use unic_langid::langid;
 pub const ID_SOURCE: &str = "EguiL10n";
 
 pub mod prelude {
-    pub use crate::{ContextExt, Localization, ResponseExt, UiExt};
+    pub use crate::{ContextExt, Localization, UiExt};
 }
 
 pub mod ui;
@@ -14,4 +14,4 @@ pub mod ui;
 mod context;
 mod localization;
 mod r#macro;
-mod response;
+// mod response;
